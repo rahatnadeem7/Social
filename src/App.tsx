f2bfdeb17bcc1c45486ff86e524bc1f1d58eba13
Rahat Nadee <rahatnadeem.24cs@licet.ac.in>
2023-11-23 21:33:14 +0530

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import Authlayout from './_auth/AuthLayout';
+import RouteLayout from './_root/RouteLayout';
+import { Toaster } from "@/components/ui/toaster"
+import {
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+           {}
+           <Route element={<Authlayout />}>
+
+          
+            <Route path="/sign-in" element={<SigninForm />} />  
+            <Route path="/sign-up" element={<SignupForm />} /> 
+            </Route>     
+           {}
+           <Route element={<RouteLayout />} >
+           <Route index element={<Home />} />
+           <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+           </Route>
+           </Routes>
+
+           <Toaster />
+           
+    </main>
+  )
+}
+
+export default App
